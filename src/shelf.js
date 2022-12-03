8a958e05403fd7ed9cc9cac1905d84dbf54afb36
Dustin Gouner <dustingouner20@gmail.com>
2022-12-03 15:04:07 -0700

    Add shelf changes

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,40 @@
+function shelfBook(book, shelfName){
+  if (shelfName.length < 3){
+    return shelfName.unshift(book)
+  }
+ 
+}
+
+function unshelfBook(book, shelfName){
+  for (var i = 0; i < shelfName.length; i++) {
+    if (book === shelfName[i].title) {
+    return shelfName.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(bookTitles){
+  var title1 = []
+  for (var i = 0; i < bookTitles.length; i++) {
+  title1.push(bookTitles[i].title)
+  }
+  return title1.join(", ")
+}
+
+function searchShelf(shelf, title){
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true
+    }
+      
+  }
+  return false
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
